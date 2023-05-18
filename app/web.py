69772a4d6d496dc3bf852bfed945d78782f7b106
Oleg-Pashchenko <pickpar@ya.ru>
2023-05-19 00:58:28 +0300

    g

--- app/web.py
+++ app/web.py
@@ -28,7 +28,7 @@ class Chrome:
         item_url = 'https://seller.ozon.ru/app/products/483801974/edit/preview'
         url = 'https://seller.ozon.ru/app/products?filter=all'
         options = uc.ChromeOptions()
-        #options.headless = True
+        options.headless = True
         self.driver = uc.Chrome(use_subprocess=True, options=options)
         self.driver.get(url)
         self.driver.maximize_window()
